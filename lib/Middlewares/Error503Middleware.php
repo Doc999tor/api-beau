71638dc2337188d524e23fbe4e2a728900a2f5c1
Victor <doc99tor@gmail.com>
2017-08-13 00:47:00 +0300

    added any route for /503 to tests

--- lib/Middlewares/Error503Middleware.php
+++ lib/Middlewares/Error503Middleware.php
@@ -8,7 +8,7 @@ class Error503Middleware {
   public function __invoke(Request $request, Response $response, callable $next) {
     	$route = $request->getAttribute('route');
 
- 		if ($route->getMethods()[0] === 'OPTIONS' || time() % 10) {
+ 		if ($route && $route->getMethods()[0] === 'OPTIONS' || time() % 10) {
 			return $next($request, $response);
  		} else {
  			return $response
--- routes.php
+++ routes.php
@@ -70,7 +70,7 @@ $app->group('/customers-details/clients', function () use ($app, $cors_settings)
 	})->add(new \Tuupola\Middleware\Cors($cors_settings));
 });
 
-$app->get('/503', function (Request $request, Response $response):Response {
+$app->any('/503', function (Request $request, Response $response):Response {
 	return $response->withHeader('Retry-After', 120)->withStatus(503);
 });
 
