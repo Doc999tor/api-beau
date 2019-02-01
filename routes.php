76e89c6da9b8618742a0946ff95a1bfd08d13294
doc999tor <doc999tor@gmail.com>
2019-02-01 03:53:55 +0200

    setPersonalData - moved to common PUT request

--- routes.php
+++ routes.php
@@ -74,7 +74,7 @@ $app->group('/customers-details', function () use ($app) {
 	$app->get('/clients', $cl_prefix . 'getClients');
 
 	$app->group('/clients/{client_id:\d+}', function () use ($app, $prefix, $cl_prefix) {
-		$app->patch('', $cl_prefix . 'setPersonalData');
+		$app->put('', $cl_prefix . 'setPersonalData');
 		$app->delete('', $cl_prefix . 'removeUser');
 		$app->post('/profile_image', $cl_prefix . 'setProfileImage');
 
