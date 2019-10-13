24e5e634496e90be65971bc8eadfe36651995e6b
doc999tor <doc999tor@gmail.com>
2019-10-13 22:46:56 +0300

    413 and 415 status added to media upload

--- lib/Controllers/CustomersDetails/MediaCtrl.php
+++ lib/Controllers/CustomersDetails/MediaCtrl.php
@@ -23,7 +23,7 @@ class MediaCtrl extends Controller {
 		} else {
 			$body = $response->getBody();
 			$body->write("<br>" . $is_body_correct['msg']);
-			return $response->withStatus(400);
+			return $response->withStatus($is_body_correct['status']);
 		}
 	}
 	public function editMediaNote (Request $request, Response $response):Response {
@@ -45,18 +45,18 @@ class MediaCtrl extends Controller {
 
 		$is_correct = true; $msg = '';
 		if (!isset($files[$file_name])) {
-			$is_correct = false; $msg .= $file_name . " is not sent or sent not under \"$file_name\" field<br>";
+			$is_correct = false; $http_status = 400; $msg .= $file_name . " is not sent or sent not under \"$file_name\" field<br>";
 		} else if ((int)$request->getHeaderLine('Content-Length') > \Lib\Helpers\Utils::returnBytes(ini_get('post_max_size'))) {
-			$is_correct = false; $msg .= 'file is too big, it should be under ' . ini_get("post_max_size") . "<br>";
+			$is_correct = false; $http_status = 413; $msg .= 'file is too big, it should be under ' . ini_get("post_max_size") . "<br>";
 		} else if (!isset($body['date']) || !\DateTime::createFromFormat('Y-m-d H:i:s', $body['date'])) {
-			$is_correct = false; $msg .= 'date has to be YYYY-MM-DD hh:mm:ss format, like  2017-12-18 02:09:54<br>';
+			$is_correct = false; $http_status = 400; $msg .= 'date has to be YYYY-MM-DD hh:mm:ss format, like  2019-12-18 02:09:54<br>';
 		}
 
 		$permitted_types = ['image/jpeg', 'image/png', 'image/webp', 'image/vnd.adobe.photoshop', 'application/pdf', 'application/ogg', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/msword', 'text/plain', 'audio/aac', 'audio/amr', 'audio/ac3', 'audio/mp3', 'audio/mp4', 'audio/mpeg', 'audio/ogg', 'audio/wav', 'audio/wave', 'audio/webm', 'audio/x-pn-wav', 'audio/x-wav', 'video/mp4', 'video/avi', 'video/ogg', 'video/webm'];
 
 		if (isset($files[$file_name]) && !in_array(mime_content_type($files[$file_name]->file), $permitted_types)) {
-			 $is_correct = false; $msg .= 'MIME type is not supported';
+			 $is_correct = false; $http_status = 415; $msg .= 'MIME type is not supported';
 		}
-		return ["is_correct" => $is_correct, "msg" => $msg];
+		return ["is_correct" => $is_correct, 'status' => $http_status, "msg" => $msg];
 	}
 }
