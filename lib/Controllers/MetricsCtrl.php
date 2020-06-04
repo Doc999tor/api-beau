d4fdb811633578ad3e0dc7a38d2f6a26841677c7
doc999tor <doc999tor@gmail.com>
2020-06-05 01:57:51 +0300

    calendar installation metrics

---
+++ lib/Controllers/MetricsCtrl.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Lib\Controllers;
+
+use Lib\Controllers\Controller as Controller;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+class MetricsCtrl extends Controller {
+	// https://api.bewebmaster.co.il/metrics/installation_popup_calendar
+	public function getCalendarInstallationAgreement (Request $request, Response $response):Response {
+		$rawBody = $request->getBody()->getContents();
+
+		$msg = ''; $is_correct = true;
+		if (empty($rawBody) || !is_array(json_decode(trim($rawBody), true)) || !count(json_decode(trim($rawBody), true))) { $msg .= '<br> body is empty or is not correct'; $is_correct = false; }
+
+		if (!$is_correct) {
+			$rawBody = $response->getBody();
+			$rawBody->write($msg);
+			return $response->withStatus(400);
+		} else { return $response->withStatus(201); }
+	}
+}
